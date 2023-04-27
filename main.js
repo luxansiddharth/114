322efa75f5a1725018247be42b9571a22865a54a
luxansiddharth <92741790+luxansiddharth@users.noreply.github.com>
2023-04-27 19:53:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+noseX=0;
+noseY=0;
+
+
+function preload()
+{
+    clown_nose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png')
+}
+
+function setup()
+{
+canvas=createCanvas(300,300);
+canvas.center();
+}
+
+
+function draw()
+{
+    
+}
